bea60a84856f657ed3ab85603a4af2415fc20614
Желудев Александр <zheludevx@gmail.com>
2021-10-16 15:36:11 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <ModeInfo.h>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <cstdio>
+#include <exception>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/program_options.hpp>
+#include <boost/optional.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
